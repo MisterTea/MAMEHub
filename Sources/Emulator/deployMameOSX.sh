af70127847c827cc84e7ab0a36640fb6e6a44544
Jason Gauci <jgmath2000@gmail.com>
2013-10-17 17:05:12 -0700

    Updated osx deploy script

--- Sources/Emulator/deployMameOSX.sh
+++ Sources/Emulator/deployMameOSX.sh
@@ -1,2 +1,3 @@
 cp csume64 ~/Programming/mame/Hub/src/java/
 scp csume64 www-data@10ghost.net:/var/www/MAMEHubDownloads/Emulators/Mac/
+ssh www-data@10ghost.net gzip -f /var/www/MAMEHubDownloads/Emulators/Mac/csume64
