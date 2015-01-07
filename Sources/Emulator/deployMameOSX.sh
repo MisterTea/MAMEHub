e21d5abf58a7803031069f8c42b2884a30af2700
Jason Gauci <jgauci@apple.com>
2015-01-07 00:16:04 -0800

    force overwrite gz file (mac)

--- Sources/Emulator/deployMameOSX.sh
+++ Sources/Emulator/deployMameOSX.sh
@@ -1,5 +1,6 @@
-scp csume64 www-data@10ghost.net:/var/www/MAMEHubDownloads/Emulators/Mac/
-ssh www-data@10ghost.net gzip -f /var/www/MAMEHubDownloads/Emulators/Mac/csume64
+gzip -k -f --best csume64
+scp csume64.gz www-data@10ghost.net:/var/www/MAMEHubDownloads/Emulators/Mac/csume64_new.gz
+ssh www-data@10ghost.net mv /var/www/MAMEHubDownloads/Emulators/Mac/csume64_new.gz /var/www/MAMEHubDownloads/Emulators/Mac/csume64.gz
 #cp csume64d ~/Programming/mame/Hub/src/java/
 #scp csume64d www-data@10ghost.net:/var/www/MAMEHubDownloads/Emulators/Mac/
 #ssh www-data@10ghost.net gzip -f /var/www/MAMEHubDownloads/Emulators/Mac/csume64d
