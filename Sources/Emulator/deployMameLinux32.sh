7ad7804962162e22ba6c2b836a177b7327f9bc1a
Jason Gauci <jgmath2000@gmail.com>
2015-01-07 00:15:07 -0800

    Overwrite gz file

--- Sources/Emulator/deployMameLinux32.sh
+++ Sources/Emulator/deployMameLinux32.sh
@@ -1,3 +1,3 @@
-gzip --best -k csume
+gzip --best -k -f csume
 scp csume.gz www-data@10ghost.net:/var/www/MAMEHubDownloads/Emulators/Linux32/csume_new.gz
 ssh www-data@10ghost.net mv /var/www/MAMEHubDownloads/Emulators/Linux32/csume_new.gz /var/www/MAMEHubDownloads/Emulators/Linux32/csume.gz
--- Sources/Emulator/deployMameLinux64.sh
+++ Sources/Emulator/deployMameLinux64.sh
@@ -1,3 +1,3 @@
-gzip --best -k csume64
+gzip --best -k -f csume64
 scp csume64.gz www-data@10ghost.net:/var/www/MAMEHubDownloads/Emulators/Linux/csume64_new.gz
 ssh www-data@10ghost.net mv /var/www/MAMEHubDownloads/Emulators/Linux/csume64_new.gz /var/www/MAMEHubDownloads/Emulators/Linux/csume64.gz 
