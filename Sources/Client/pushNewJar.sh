85d69eca2b59e10c66a6c53102f29d661719413b
Jason Gauci <jgauci@apple.com>
2015-05-28 10:34:25 -0700

    update client build script.

--- Sources/Client/pushNewJar.sh
+++ Sources/Client/pushNewJar.sh
@@ -1,5 +1,5 @@
 #mvn -U clean package
-gradle solid
+./gradlew solid
 scp solid/MAMEHubClient-0.0.1-SNAPSHOT.jar www-data@10ghost.net:/var/www/MAMEHubDownloads/Frontend/MAMEHubClient.jar
 #scp target/MAMEHubClient-0.0.1-SNAPSHOT-jar-with-dependencies.jar www-data@10ghost.net:/var/www/MAMEHubDownloads/Frontend/MAMEHubClient.jar
 ssh www-data@10ghost.net gzip -f --best /var/www/MAMEHubDownloads/Frontend/MAMEHubClient.jar
