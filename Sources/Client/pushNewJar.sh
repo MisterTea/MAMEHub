b4318352b2f5b8975b313415f8db9074548682b0
Jason Gauci <jgauci@apple.com>
2015-10-21 23:57:06 -0700

    Change push script for new server.

--- Sources/Client/pushNewJar.sh
+++ Sources/Client/pushNewJar.sh
@@ -1,6 +1,3 @@
-#mvn -U clean package
 ./gradlew solid
-scp solid/MAMEHubClient-0.0.1-SNAPSHOT.jar www-data@10ghost.net:/var/www/MAMEHubDownloads/Frontend/MAMEHubClient.jar
-#scp target/MAMEHubClient-0.0.1-SNAPSHOT-jar-with-dependencies.jar www-data@10ghost.net:/var/www/MAMEHubDownloads/Frontend/MAMEHubClient.jar
-#ssh www-data@10ghost.net gzip -f --best /var/www/MAMEHubDownloads/Frontend/MAMEHubClient.jar
-ssh www-data@10ghost.net "gzip -c --best /var/www/MAMEHubDownloads/Frontend/MAMEHubClient.jar > /var/www/MAMEHubDownloads/Frontend/MAMEHubClient.jar.gz"
+scp solid/MAMEHubClient-0.0.1-SNAPSHOT.jar pawn@10ghost.net:/var/www/html/MAMEHubDownloads/Frontend/MAMEHubClient.jar
+ssh pawn@10ghost.net "gzip -c --best /var/www/html/MAMEHubDownloads/Frontend/MAMEHubClient.jar > /var/www/html/MAMEHubDownloads/Frontend/MAMEHubClient.jar.gz"
